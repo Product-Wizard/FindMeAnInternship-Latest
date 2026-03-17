a2acdbc29e36d95f25d46dc4c9ab4397745552bd
Product-Wizard <159847551+Product-Wizard@users.noreply.github.com>
2026-03-17 16:29:25 +0100

    Escape JSON-LD before injecting structured data

--- pages/JobDetailsPage.tsx
+++ pages/JobDetailsPage.tsx
@@ -234,10 +234,15 @@ function JobPostingStructuredData({
     url,
   };
 
+  const escapedJsonLd = JSON.stringify(jobPostingJsonLd)
+    .replace(/</g, "\\u003c")
+    .replace(/>/g, "\\u003e")
+    .replace(/&/g, "\\u0026");
+
   return (
     <script
       type='application/ld+json'
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(jobPostingJsonLd) }}
+      dangerouslySetInnerHTML={{ __html: escapedJsonLd }}
     />
   );
 }
